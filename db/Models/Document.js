784db99be6e72bdfd5c9284b40bab4b99bd5f084
Vincent Barilla <vbarilla@gmail.com>
2016-09-27 15:19:02 -0700

    Fix small bug in Document

--- db/Models/Document.js
+++ db/Models/Document.js
@@ -1,8 +1,9 @@
 var Sequelize = require('sequelize');
 var sqlConnection = require('../config.js');
 
-var Document = Sequelize.define('document', 
-  {  }
+var Document = sqlConnection.define('document', 
+  { textS3: Sequelize.STRING, 
+    sharelink: Sequelize.STRING }
 );
 
 Document.sync();
