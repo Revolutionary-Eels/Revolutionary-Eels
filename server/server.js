34b0ff248961f2ecd29b9a782e5655e59a3d0e2f
Vincent Barilla <vbarilla@gmail.com>
2016-09-27 11:25:51 -0700

    Debug route to index in client

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/client/public'));
+app.use(express.static(__dirname + '/../client'));
 
 app.get('/', function(req, res) {
   res.sendFile('index');
